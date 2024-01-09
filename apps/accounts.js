adf9c5a2cc000c88ebe07fa75bcb96ce18ce5f58
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-09 15:04:25 +0700

    feat: add completed exercise

---
+++ apps/accounts.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+import { comments as commentsFromFile } from "../data/comments.js";
+
+let comments = [...commentsFromFile];
+
+const accountRouter = Router();
+
+accountRouter.get("/", function (req, res) {
+    res.send("View all accounts");
+});
+
+accountRouter.get("/:id", function (req, res) {
+    res.send("View an account by id");
+});
+
+accountRouter.post("/", function (req, res) {
+    res.send("Create an account");
+});
+
+accountRouter.put("/:id", function (req, res) {
+    res.send("Update an account by id");
+});
+
+accountRouter.delete("/:id", function (req, res) {
+    res.send("Delete an account by id");
+});
+
+export default accountRouter;
